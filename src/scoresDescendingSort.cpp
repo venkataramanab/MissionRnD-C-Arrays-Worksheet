85d054301048ad65412bca906bb697dccc7e6464
venkat <venkat>
2015-12-07 21:34:45 +0530

    Submit Commit

--- src/scoresDescendingSort.cpp
+++ src/scoresDescendingSort.cpp
@@ -14,30 +14,56 @@ NOTES:
 */
 
 #include <stdio.h>
-
 struct student {
 	char name[10];
 	int score;
 };
-
+struct student* quickSort(struct student *arr, int l, int h);
+void swap(int* p1, int* p2);
+int partition(struct student *Arr, int left, int right);
 void * scoresDescendingSort(struct student *students, int len) {
-	struct student temp;
-	if ((students == NULL)||(len<0)){
+	if ((students)&&(len>0)){
+		if (len == 1){
+			return students;
+		}
+		else{//quick sort
+			return quickSort(students, 0, len - 1);
+		}
+	}
+	else{
 		return NULL;
 	}
-	else if (len == 1){
-		return students;
-	}
-	else{//selection sort
-		for (int i = 0; i < len; i++){
-			for (int j = i; j < len; j++){
-				if (students[i].score < students[j].score){
-					temp = students[i];
-					students[i] = students[j];
-					students[j] = temp;
-				}
-			}
-		}
-		return students;
-	}
 }
+struct student* quickSort(struct student *Arr, int left, int right)
+{
+	if (left < right)
+	{
+		int p = partition(Arr, left, right);
+		quickSort(Arr, left, p - 1);
+		quickSort(Arr, p + 1, right);
+	}
+	return Arr;
+}
+int partition(struct student *Arr, int left, int right)
+{
+	int temp = Arr[right].score;
+	int i = (left - 1);
+
+	for (int j = left; j <= right - 1; j++)
+	{
+		if (Arr[j].score >= temp)
+		{
+			i++;
+			swap(&Arr[i].score, &Arr[j].score);
+		}
+	}
+	swap(&Arr[i + 1].score, &Arr[right].score);
+	return (i + 1);
+}
+void swap(int* p1, int* p2)
+{
+	int t = *p1;
+	*p1 = *p2;
+	*p2 = t;
+}
+
